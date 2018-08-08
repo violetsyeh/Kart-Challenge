a58388a238b70d49a880c0c2a2924750a4ad70fd
Violet Yeh <violetsyeh@gmail.com>
2018-08-07 23:54:32 -0700

    Added server, gitignore, requirements, templates/index

---
+++ server.py
@@ -0,0 +1,35 @@
+from flask_debugtoolbar import DebugToolbarExtension
+
+from flask import (Flask, render_template, redirect, request, flash,
+                   session, jsonify)
+
+from model import Product, connect_to_db, db
+
+app = Flask(__name__)
+
+app.secret_key = "ABC"
+
+@app.route('/')
+def index():
+	"""Homepage."""
+
+	return render_template("index.html")
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+
+    app.run(port=5000, host='0.0.0.0')

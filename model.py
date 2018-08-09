c537c2c43cd03124b87da2dcb7f96cdf48569390
Violet Yeh <violetsyeh@gmail.com>
2018-08-09 00:01:09 -0700

    added comments to model.py

--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+"""Utility file to seed items into database"""
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
@@ -5,6 +6,7 @@ import os
 
 
 db = SQLAlchemy()
+"""Attempted to create API but was having difficulty"""
 # app = Flask(__name__)
 # ma = Marshmallow(app)
 # basedir = os.path.abspath(os.path.dirname(__file__))
@@ -25,7 +27,7 @@ class Product(db.Model):
 
 		return "<Product product_id={} title={}".format(self.product_id, self.title)
 
-
+"""Attemtped to make route for API"""
 # @app.route("/suggestions", methods=["GET"])
 # def get_product():
 # 	all_products = User.query.all()
@@ -36,11 +38,6 @@ class Product(db.Model):
 #     app.run(debug=True)
 
 
-
-# class Kart(db.Model):
-# 	"""Productss in kart"""
-
-# 	__tablename__ = 'karts'
 	
 
 
@@ -49,7 +46,7 @@ def connect_to_db(app):
 	"""Connect the database to Flask app."""
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///kart'
-	# app.config['SQALCHMEY_TRACK_MODIFICATIONS'] = False 
+	app.config['SQALCHMEY_TRACK_MODIFICATIONS'] = False 
 	db.app = app
 	db.init_app(app)
 
@@ -57,6 +54,4 @@ if __name__ == "__main__":
 
 	from server import app
 	connect_to_db(app)
-	# app.run(debug=True)
-	# app.run(port=5000, host='0.0.0.0')
 	print "Connected to DB."

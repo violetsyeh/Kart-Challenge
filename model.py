674c72e32f90c6486ebabf8c0204c863c2036aff
Violet Yeh <violetsyeh@gmail.com>
2018-08-08 18:33:29 -0700

    Having problems with api, working on drop down search for items

--- model.py
+++ model.py
@@ -1,11 +1,19 @@
-"""Model and database function for Kart"""
-
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
 
 db = SQLAlchemy()
+# app = Flask(__name__)
+# ma = Marshmallow(app)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///kart' + os.path.join(basedir, 'model.postgresql')
+# app.config['SQALCHMEY_TRACK_MODIFICATIONS'] = False 
+
 
 class Product(db.Model):
-	"""Products of Kart"""
+	"""Products"""
 
 	__tablename__ = 'products'
 
@@ -18,11 +26,30 @@ class Product(db.Model):
 		return "<Product product_id={} title={}".format(self.product_id, self.title)
 
 
+# @app.route("/suggestions", methods=["GET"])
+# def get_product():
+# 	all_products = User.query.all()
+# 	print all_products
+# 	return jsonify(result.data)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
+
+
+
+# class Kart(db.Model):
+# 	"""Productss in kart"""
+
+# 	__tablename__ = 'karts'
+	
+
+
+
 def connect_to_db(app):
 	"""Connect the database to Flask app."""
 
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///kart'
-	app.config['SQALCHMEY_TRACK_MODIFICATIONS'] = False 
+	# app.config['SQALCHMEY_TRACK_MODIFICATIONS'] = False 
 	db.app = app
 	db.init_app(app)
 
@@ -30,4 +57,6 @@ if __name__ == "__main__":
 
 	from server import app
 	connect_to_db(app)
+	# app.run(debug=True)
+	# app.run(port=5000, host='0.0.0.0')
 	print "Connected to DB."
